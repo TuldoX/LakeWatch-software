1e1680a052ea48d20eea1889d25497228ed9dd0c
TuldoX <matusmartiska24@gmail.com>
2025-12-29 12:53:29 +0100

    pages in router

--- frontend/lakewatch/src/router/index.js
+++ frontend/lakewatch/src/router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '@/views/Dashboard.vue'
 import Device from '@/views/Device.vue'
+import Notifications from '@/views/Notifications.vue'
+import Settings from '@/views/Settings.vue'
 
 const routes = [
   { path: '/', name: 'Dashboard', component: Dashboard },
   { path: '/device', name: 'Device_details', component: Device},
+  { path: '/notifications', name: 'Notifications', component: Notifications},
+  { path: '/settings', name: 'Settings', component: Settings}
 ]
 
 const router = createRouter({
--- frontend/lakewatch/src/views/Settings.vue
+++ frontend/lakewatch/src/views/Settings.vue
@@ -1,8 +1,7 @@
 <script setup>
 </script>
 <template>
-  <h1 class="page-title">Probes</h1>
+  <h1>Settings</h1>
 </template>
-
 <style scoped>
 </style>
